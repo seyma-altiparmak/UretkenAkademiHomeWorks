164414098f47db70371d42c3b3f261772d5c2682
Seyma Altiparmak <seyma.altiparmak@ogr.sakarya.edu.tr>
2024-01-22 18:09:47 +0300

    UI

--- Assets/Scenes/Week7/Scripts/Destroyer.cs
+++ Assets/Scenes/Week7/Scripts/Destroyer.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public string parentName;
+   
     void Update()
     {
-        
+        parentName = transform.name;
+        StartCoroutine((DestroyClone()));
+    }
+
+    IEnumerator DestroyClone()
+    {
+        yield return new WaitForSeconds(10f);
+        if (parentName == "Section(Clone)")
+        {
+            Destroy(gameObject);
+        }
     }
 }
