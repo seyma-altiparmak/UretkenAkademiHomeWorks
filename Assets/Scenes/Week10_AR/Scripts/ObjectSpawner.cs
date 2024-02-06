3fa79513767aa77391e0a8ccb9a1941c5c5f57d1
Şeyma Altıparmak <seyma.altiparmak@ogr.sakarya.edu.tr>
2024-02-06 22:03:18 +0300

    AugmentedRealityLessonsCode

---
+++ Assets/Scenes/Week10_AR/Scripts/ObjectSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private PlacementIndicator placementIndicator;
+
+    private void Start()
+    {
+        placementIndicator = FindAnyObjectByType<PlacementIndicator>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(objectToSpawn, 
+                placementIndicator.transform.position, 
+                placementIndicator.transform.rotation);
+        }
+
+    }
+}
